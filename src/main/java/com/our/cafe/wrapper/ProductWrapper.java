282c42487332d96e03cee1cccdc23127991e02bc
Anishaaa123 <Anishaaa123@gmail.com>
2024-01-11 17:20:27 +0530

    Cafe Management System

---
+++ src/main/java/com/our/cafe/wrapper/ProductWrapper.java
@@ -0,0 +1,86 @@
+package com.our.cafe.wrapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductWrapper {
+	private String description;
+    private String name;
+    private Integer price;
+    private String status;
+    private Integer categoryId;
+    private String categoryName;
+    
+	private Integer id;
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+    public ProductWrapper(Integer id, String description, String name, Integer price, String status, Integer categoryId,
+			String categoryName) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.name = name;
+		this.price = price;
+		this.status = status;
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+    public ProductWrapper(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ProductWrapper(Integer id, String name, String description, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+   
